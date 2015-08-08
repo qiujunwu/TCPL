ba3a6c317a4079619ffe905b084aaa4fb368b1b6
linux775 <qiujunwu775@gmail.com>
2015-08-09 01:21:55 +0800

    test

--- chapter2/section2.8/main.c
+++ chapter2/section2.8/main.c
@@ -1,13 +1,33 @@
 
 #include <stdio.h>
 
-void squeeze(char s[], int c);
+/* squeeze: delete all c from s */
+void squeeze(char s[], int c){
+    int i, j;
+
+    for (i = j = 0; s[i] != '\0'; i++){
+        if (s[i] != c)
+            s[j++] = s[i];
+    }
+    s[j] = '\0';
+}
+
+/* strcat: concatenate t to end of s; s must big enough */
+void strcat(char s[], char t[]){
+    int i, j;
+
+    i = j = 0;
+    while (s[i] != '\0')    /* find end of s */
+        i++;
+    while ((s[i++] = t[j++]) != '\0')   /* copy t */
+        ;
+}
 main(){
     char s[] = "This is a test string!";
     squeeze(s, 's');
 }
 
-/* squeeze: delete all c from s */
+
 void squeeze(char s[], int c){
     int i, j;
 
